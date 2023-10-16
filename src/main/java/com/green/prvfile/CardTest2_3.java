14034ec6a87cc8263c1ecf5a70f629d5cca4d1c3
이영웅 <hw05045@naver.com>
2023-10-16 21:32:09 +0900

    1016-백앤드

--- src/main/java/com/green/prvfile/CardTest2_3.java
+++ src/main/java/com/green/prvfile/CardTest2_3.java
@@ -4,7 +4,7 @@ class Card4{
     String kind;
 }
 public class CardTest2_3 {
-    public static void main(String[] args) {// 1. 카드를 만든다 2. 출력한다
+    public static void main(String[] args) {// 1. 카드를 만든다 2. 출력한
         Card4 c = new Card4();
     }
 }
